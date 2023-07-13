92ea6a47aa9951da9cbb194c4b553f0aa64afa71
gammer1 <kiptookelvin808@gmail.com>
2023-07-13 12:20:27 +0300

    java.commit

--- external.js
+++ external.js
@@ -15,18 +15,42 @@ function ready(){
         button.addEventListener("click",addToCartClicked)
     }
 
-    function addToCartClicked(){
-        alert("button clicked")
-        var bu=event,target
-        var shopitem=bu.parentElement.parentElement
-        var title=document.getElementsByClassName("shop-item-title")[0].innerText
-        var price=document.getElementsByClassName("shop-item-prize")[0].innerText
-        var image=document.getElementsByClassName("shop-item-image")[0].src
-        addItemToCart(title,price,Image)
-    }
-    function addItemToCart(){
-    var cartrow=document.createElement("div")
-    carteow.classlist.add("cart-row")
-    var cartitems=document.getElementsByClassName("")
-    }
+    var removecartitem=document.getElementsByClassName("btn-remove")
+    for(i=0;i<removecartitem.length;i++)
+        removecartitem.addEventListener("click",removecartitem)
 }
+
+function removecartitem(event){
+    var buttoonclicked=event.target
+    buttoonclicked.parentElement.parentElement.remove()
+}
+
+    function addToCartClicked(event){
+        var button=event.target
+        var shopitem=button.parentElement.parentElement
+        var title=shopitem.getElementsByClassName("shop-item-title")[0].innerText
+        var price=shopitem.getElementsByClassName("shop-item-price")[0].innerText
+        var imagesrc=shopitem.getElementsByClassName("shop-item-image")[0].src
+        addItemToCart(title,price,imagesrc)
+    }
+    function addItemToCart(title,price,imagesrc){
+    var cartrow=document.createElement("div")
+    cartrow.classList.add("cart-row")
+    var cartitems=document.getElementsByClassName("cart-items")[0]
+
+    var cartrowcontents=`
+    <div class="cart-item cart-column">
+        <img class="cart-item-image" src=${imagesrc} width="100" heoght="100">
+            <span class="cart-item-title">${title}</span>
+    </div>
+   <span class="cart-price">${price}</span>
+<div>
+    <input class="cart-quantity-input" type="number" value="1">
+    <button class="btn-remove" type="button">Remove</button>
+</div>
+`
+cartrow.innerHTML=cartrowcontents
+cartitems.append(cartrow)
+cartrow.getElementsByClassName("btn-danger")[0].addEventListener("click",removecartitem)
+cartrow.getElementsByClassName("cart-quantity-input")[0].addEventListener("click", quantityChanged)
+    }
